2e26cf75bef97d51e2008c773ac92e5294142507
sometime <bing2619@163.com>
2019-10-18 10:48:40 +0800

    修改配置

--- siteinfo.js
+++ siteinfo.js
@@ -2,6 +2,6 @@
  * 配置文件
  */
 module.exports = {
-  name: "萤火小程序商城",
-  siteroot: "http://yoshop.cn/", // 必填: api地址，结尾要带/
+  name: "一叶小程序商城",
+  siteroot: "http://www.php.com/", // 必填: api地址，结尾要带/
 };
